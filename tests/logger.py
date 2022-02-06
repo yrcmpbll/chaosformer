518f4dd93fcac3b5eab4ce76f4bb5dc1fd7a4c74
yrcmpbll <yuri.campbell@gmail.com>
2022-02-06 15:41:44 +0100

    make folder when not found

--- tests/logger.py
+++ tests/logger.py
@@ -1,3 +1,6 @@
+import inspect
+import os
+
 LOGFOLDER = './.logs/'
 
 
@@ -12,5 +15,25 @@ class Logger:
         self.logs.append(s+'\n')
     
     def write(self):
-        with open(LOGFOLDER+'array_size.log', mode='w') as f:
+        CHECK_FOLDER = os.path.isdir(LOGFOLDER)
+        if not CHECK_FOLDER:
+            os.makedirs(LOGFOLDER)
+
+        with open(LOGFOLDER+self.filename, mode='w') as f:
             f.writelines(self.logs)
+
+
+class TestLogger(Logger):
+
+    def __init__(self, filename=None) -> None:
+        if filename is not None:
+            self.filename = filename
+        else:
+            caller_name = inspect.stack()[1][3]
+            functionality_name = '_'.join(caller_name.split('_')[1:])
+            self.filename = 'log_'+functionality_name+'.log'
+
+            # print(inspect.stack()[0][3])
+            # print(inspect.stack()[1][3])  # will give the caller of foos name, if something called foo
+
+        super().__init__(filename=self.filename)
